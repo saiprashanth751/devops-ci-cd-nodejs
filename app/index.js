8ec11854dfaac0933df09f2608e761e8c034c20e
Sai Prashanth <saiprashanth751@gmail.com>
2025-12-15 20:29:54 +0530

    Adding node project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("DevOps CI/CD Pipeline is running 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
